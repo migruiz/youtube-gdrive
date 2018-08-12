ac3e8190a3f92e610c3e0a964fad94ea566b6d8a
Miguel Ruiz <mig.ruiz@gmail.com>
2018-08-12 01:06:27 +0100

    alexa command

--- extract.sh
+++ extract.sh
@@ -1,5 +1,10 @@
-youtube-dl --download-archive downloaded.txt --no-overwrites -ic --yes-playlist -o '%(title)s.%(ext)s' --restrict-filenames  --extract-audio --audio
--format mp3 --audio-quality 0 --socket-timeout 5 https://www.youtube.com/playlist?list=PLJLM5RvmYjvxaMig-iCqA9ZrB8_gg6a9g
+youtube-dl  -ic   --extract-audio --audio -format mp3 --audio-quality 0 --socket-timeout 5 https://www.youtube.com/watch?v=zbajp0zS3Pk
+youtube-dl  -ic   --extract-audio --audio -format mp3 --audio-quality 0 --socket-timeout 5 https://www.youtube.com/watch?v=zbajp0zS3Pk
 
 
 docker run -it --rm -v "/home/pi/gdrive:/App/security" -v "/home/pi/files:/App/files" migruiz/youtube-gdrive
+
+
+youtube-dl    --extract-audio --audio-format mp3 --audio-quality 192K https://www.youtube.com/watch?v=lDj
+ri2d2X6Q
+
