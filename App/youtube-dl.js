f1850373f375823942e30e14d42c081e60dcf45c
Miguel Ruiz <miguel@dpsystems.ie>
2018-08-13 15:53:34 +0100

    preparing  command

---
+++ App/youtube-dl.js
@@ -0,0 +1,37 @@
+var spawn = require('child_process').spawn;
+
+
+function execyoutubedlAsync(videourl,outputfile){
+    return new Promise(function (resolve, reject) {
+    const youtubedlProcess = spawn(ffmpegFolder+'youtube-dl'
+            , [
+                '--extract-audio',
+                '--audio-format',
+                'mp3',
+                '--audio-quality',
+                '192K',
+                '-o',
+                '%(id)s.%(ext)s',
+                '--restrict-filenames',
+                videourl,
+            ]);
+
+        var result = '';
+        ffprobe.stdout.on('data', (data) => {
+            result += data.toString();
+        });
+        ffprobe.stderr.on('data', (data) => {
+            return reject(err);
+        });
+        ffprobe.on('exit', function (code, signal) {
+            resolve();
+        });
+    });
+}
+
+
+exports.downloadVideoAsync=async function(videoId){
+    var videourl='https://www.youtube.com/watch?v='+videoId;
+    await execyoutubedlAsync(videourl);
+    
+}
