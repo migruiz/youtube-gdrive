56def458c1d7c7945edb27366c8615d9496a7187
Miguel Ruiz <miguel@dpsystems.ie>
2018-08-17 14:17:15 +0100

    refactoring code

--- App/app.js
+++ App/app.js
@@ -17,6 +17,19 @@ function getItemsToDelete(savedItems,currentItems){
   });
   return itemsToDelete;
 }
+
+function getFirstNewItem(savedItems,currentItems){
+  var savedItemsIds = savedItems.map(a => a.id);
+  var newItems=[];
+  currentItems.forEach(currentItem => {
+    if (!savedItemsIds.includes(currentItem.id)){
+      return currentItem;
+    }
+  });
+  return null;
+}
+
+
 function getItemById(items,id){
   for (let index = 0; index < items.length; index++) {
     const item = items[index];
@@ -26,26 +39,33 @@ function getItemById(items,id){
   return null;
 }
 
-var previousYoutubePlayListHash;
+var _previousYoutubePlayListHash;
 var syncFx=async ()=>{
   var playlistId=process.env.PLAYLISTID;
   var currentItems=await youtube.getPlaylistinfoAsync(playlistId);
   var currentYoutubePlayListHash=hash(currentItems);
-  if (previousYoutubePlayListHash && previousYoutubePlayListHash===currentYoutubePlayListHash){
+  if (_previousYoutubePlayListHash && _previousYoutubePlayListHash===currentYoutubePlayListHash){
     setTimeout(() => {
       syncFx();
     }, 1000);
     return;
   }
-  previousYoutubePlayListHash=currentYoutubePlayListHash;
   var savedItems=await dynamo.getyoutubePlaylistAsync(playlistId);
-  await deleteItems(savedItems, currentItems);
-  await updateAndAddNewItems(currentItems, savedItems);
-  await dynamo.updateyoutubePlaylistAsync(playlistId,currentItems);
-  neededToSyncWithDropBox=await syncWithDropbox(currentItems);
-  if (neededToSyncWithDropBox){
-    await dynamo.updateyoutubePlaylistAsync(playlistId,currentItems);
+  
+  var itemsToDelete = getItemsToDelete(savedItems, currentItems);
+  await deleteItems(itemsToDelete);
+  removeDeletedItemsFromSavedList(savedItems,itemsToDelete);
+
+
+  var newItem=getFirstNewItem(savedItems,currentItems);
+  if (newItem){
+    await syncNewItem(newItem);
+    savedItems.push(newItem);
   }
+  await dynamo.updateyoutubePlaylistAsync(playlistId,savedItems);
+
+  
+  _previousYoutubePlayListHash=currentYoutubePlayListHash;
 
   setTimeout(() => {
     syncFx();
@@ -55,78 +75,29 @@ var syncFx=async ()=>{
 syncFx();
 
 
-async function syncWithDropbox(currentItems){
-  var neededToSyncWithDropBox=false;
-  for (let index = 0; index < currentItems.length; index++) {
-    const currentItem = currentItems[index];
-    if (currentItem.hostingAt!=='dropbox'){
-      var dropboxurl=await dropbox.uploadToDropbox(currentItem.bucketFileName,currentItem.url);
-      currentItem.url=dropboxurl;
-      currentItem.hostingAt='dropbox';
-      neededToSyncWithDropBox=true;
-    }
-  }
-  return neededToSyncWithDropBox;
+function removeDeletedItemsFromSavedList(savedItems,itemsToDelete){
+  itemsToDelete.forEach(itemToDelete => {    
+    savedItems.splice( savedItems.indexOf(itemToDelete), 1 );
+  });
 }
 
 
-async function deleteItems(savedItems, currentItems) {
-  var itemsToDelete = getItemsToDelete(savedItems, currentItems);
+async function deleteItems(itemsToDelete) {
   for (let index = 0; index < itemsToDelete.length; index++) {
-    const itemToDelete = itemsToDelete[index];
-    try{
-      await firebase.deleteFileAsync(itemToDelete.bucketFileName);
-    }catch(error)
-    {
-      console.log(error);
-    }
-    try{
       await dropbox.deleteAsync(itemToDelete.bucketFileName);
-    }
-    catch (error) {
-      console.log(error);
-    }
   }
 }
 
-async function updateAndAddNewItems(currentItems, savedItems) {
-  var itemsWithError = await processAndGetItemsWithError();
-  for (let index = 0; index < itemsWithError.length; index++) {
-    const itemWithError = itemsWithError[index];
-    remove(currentItems,itemWithError);
-  }
-  function remove(array, element) {
-    const index = array.indexOf(element);
-    array.splice(index, 1);
-  }
-  
-
-  async function processAndGetItemsWithError() {
-    var itemsWithError = [];
-    for (let index = 0; index < currentItems.length; index++) {
-      const currentItem = currentItems[index];
-      var savedItem = getItemById(savedItems, currentItem.id);
-      if (savedItem) {
-        currentItem.url = savedItem.url;
-        currentItem.bucketFileName = savedItem.bucketFileName;
-        currentItem.hostingAt=savedItem.hostingAt;
-      }
-      else {
-        try {
-          var localFile = await youtubedl.downloadVideoAsync(currentItem.id);          
-          var result = await firebase.uploadFileAsync(localFile);
-          currentItem.url = result.downloadurl;
-          currentItem.bucketFileName = result.fileName;
-          currentItem.hostingAt='firebase';
-        }
-        catch (error) {
-          console.log(JSON.stringify(error));
-          itemsWithError.push(currentItem);
-        }
-      }
-    }
-    return itemsWithError;
-  }
+async function syncNewItem(item){
+  var localFile = await youtubedl.downloadVideoAsync(item.id);          
+  var firebaseResult = await firebase.uploadFileAsync(localFile);
+  var dropboxurl=await dropbox.uploadToDropbox(firebaseResult.fileName,firebaseResult.downloadurl);
+  await firebase.deleteFileAsync(firebaseResult.fileName);
+  item.url=dropboxurl;
+  item.bucketFileName=firebaseResult.fileName;
 }
+
+
+ 
 
 
